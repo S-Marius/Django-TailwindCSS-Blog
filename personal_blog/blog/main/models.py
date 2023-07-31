fbdb503c3d3f53407eb5ce7feb7134ffb91c85be
Marius <121916282+S-Marius@users.noreply.github.com>
2023-07-31 20:33:14 +0200

    Update models.py

--- personal_blog/blog/main/models.py
+++ personal_blog/blog/main/models.py
@@ -27,10 +27,11 @@ class BlogPost(models.Model):
         blank=True,  # Allow the field to be empty (blank)
     )
 
+    # Overwrite of the default save function
     def save(self, *args, **kwargs):
         if not self.slug:
             self.slug = str(uuid.uuid4())  # Generate a new UUID for the slug
-        super(BlogPost, self).save(*args, **kwargs)
+        super(BlogPost, self).save(*args, **kwargs) # First we are creating and adding the slug to itself if it doesn't exist, then we use the default save function provided by Django.
 
     def delete(self, *args, **kwargs):
         # Delete the associated thumbnail image if it exists
@@ -47,3 +48,6 @@ def delete_thumbnail_on_blogpost_delete(sender, instance, **kwargs):
     if instance.thumbnail and instance.thumbnail.name != BlogPost._meta.get_field('thumbnail').get_default():
         # Delete the associated thumbnail image if it exists and is not the default thumbnail
         instance.thumbnail.delete(False)
+
+        # the 'False' argument of the .delete function is not what you might expect. In fact, this is what it does:
+        # The argument False is passed to the delete() method to indicate that the model instance should not be saved after the thumbnail deletion.
