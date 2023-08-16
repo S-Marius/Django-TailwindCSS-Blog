4c869b14770ea8bc9bc9571b599bdc8a3e9a2940
Marius <121916282+S-Marius@users.noreply.github.com>
2023-08-16 20:25:24 +0200

    Add files via upload

---
+++ personal_blog/blog/urls.py
@@ -0,0 +1,14 @@
+from django.contrib import admin
+from django.urls import include, path
+from django.conf import settings
+from django.conf.urls.static import static
+
+urlpatterns = [
+    path("", include("main.urls")),
+    path("admin/", admin.site.urls),
+    # path("__debug__/", include("debug_toolbar.urls")),
+]
+
+# Serve media files during development
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
